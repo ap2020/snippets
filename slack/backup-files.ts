9d2e43e9af5168ee9a42c1d504dcbd21770682d0
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2020-06-22 02:34:37 +0900

    slack/backup-files: save fileId

--- slack/backup-files.ts
+++ slack/backup-files.ts
@@ -5,21 +5,31 @@
 
 import axios from 'axios';
 import type {Readable} from 'stream';
+import {promises as fs} from 'fs';
+import path from 'path';
+import ora from 'ora'
 import {webClient} from '../utils/slack';
-import fs from 'fs';
 import {google} from 'googleapis';
 import {getGoogleClient} from '../utils/google';
 import {driveFolderId} from './backup-files.secret';
 
 type File = {
+    id: string;
     name: string;
     mimetype: string;
     url_private_download: string;
 };
 
+const objToMap = <T>(obj: {[key: string]: T}): Map<string, T> => new Map(Object.entries(obj));
+const mapToObj = <T>(map: Map<string, T>): {[key: string]: T} => Object.fromEntries(map.entries());
+
+const resPath = path.join(__dirname, 'backup-files-res.secret.json');
+
 const main = async () => {
     const auth = await getGoogleClient();
     const drive = google.drive({version: 'v3', auth});
+    const fileIdMap = objToMap(JSON.parse(await fs.readFile(resPath, {encoding: 'utf-8'})));
+    
     const {files}: {files: File[]} = await webClient.files.list({
         count: 1,
     }) as any;
@@ -34,7 +44,7 @@ const main = async () => {
             responseType: 'stream',
         }
     )).data;
-    const driveFile = await drive.files.create({
+    const driveFile = (await drive.files.create({
         requestBody: {
             name: file.name,
             mimeType: file.mimetype,
@@ -44,7 +54,10 @@ const main = async () => {
             mimeType: file.mimetype,
             body: fileStream,
         },
-    });
+    })).data;
+    fileIdMap.set(file.id, driveFile.id);
+
+    await fs.writeFile(resPath, JSON.stringify(mapToObj(fileIdMap), undefined, 2));
     console.log('done!');
 };
 
