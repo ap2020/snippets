97edeebe45eff711e06cd68082184ca988a7c874
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2020-09-20 00:08:45 +0900

    new_semester/scrape_syllabus: scrape all courses

--- new_semester/scrape_syllabus.ts
+++ new_semester/scrape_syllabus.ts
@@ -2,7 +2,7 @@ import puppeteer from 'puppeteer';
 import {execFile} from 'child_process';
 import {promisify} from 'util';
 import ora from 'ora';
-import {courses, gakubuCode} from './scrape_syllabus.secret'
+import {gakkas, gakubuCode} from './scrape_syllabus.secret'
 import {promises as fs} from 'fs';
 import path from 'path';
 
@@ -12,8 +12,17 @@ const getInnerText = async (e: puppeteer.ElementHandle<any>): Promise<string> =>
 const getPassword = async (): Promise<string> =>
     (await promisify(execFile)('security', ['find-generic-password', '-s', process.env.KEYCHAIN_UTAS ,'-w'])).stdout.replace(/\n$/, '');
 
+const fileExists = async (filepath: string): Promise<boolean> => {
+    try {
+        await fs.access(filepath);
+        return true;
+    } catch (e) {
+        return false;
+    }
+}
+
 (async () => {
-    const browser = await puppeteer.launch(/*{headless: false, slowMo: 0}*/);
+    const browser = await puppeteer.launch({headless: false, slowMo: 0});
     const page = await browser.newPage();
     
     let sp1 = ora('Logging in').start();
@@ -32,26 +41,17 @@ const getPassword = async (): Promise<string> =>
     await page.waitFor('#tab-sy', {timeout: 60 * 1000});
     sp1.succeed('Logged in');
 
-    for (const {gakubu, gakka, courseName} of courses) {
+    for (const {gakubu, gakka} of gakkas) {
+        let sp2 = ora({text: 'opening syllabus page', indent: 1}).start();
         try {
-            let fileExists = true;
-            try {
-                await fs.access(path.join(__dirname, 'syllabus_data_secret', `${courseName}.json`));
-                console.log(`Skipping existing file ${gakubu} > ${gakka} > ${courseName}`)
-                continue;
-            } catch (e) {
-            }
-            console.log(`Scraping ${gakubu} > ${gakka} > ${courseName}`);
-            let sp2 = ora({text: 'opening syllabus page', indent: 1}).start();
             await page.waitFor(1000);
             await page.click('#tab-sy');
-
+            
             await page.waitFor(1000);
             sp2.text = 'Opening department list';
             await page.waitFor('#tabmenu-ul > li:nth-child(3) > span', {timeout: 60 * 1000});
             await page.waitFor(1000);
             await page.click('#tabmenu-ul > li:nth-child(3) > span')
-            
             
             sp2.text = 'Select department';
             await page.waitFor('#main-frame-if', {timeout: 60 * 1000}); 
@@ -60,16 +60,14 @@ const getPassword = async (): Promise<string> =>
             await frame.waitFor('#gakubuCode', {timeout: 60 * 1000});
             await page.waitFor(1000);
             await frame.select('#gakubuCode', gakubuCode[gakubu]);
-
+            
             await page.waitFor(1000);
             await frame.waitFor('#gakkaCourseDataForm', {timeout: 60 * 1000});
             await page.waitFor(1000);
             sp2.text = 'Selecting department';
             await (await frame.$x(`//*[@id="gakkaCourseDataForm"]/table/tbody/tr/td/a[contains(text(), "${gakka}")]`))[0].click();
 
-            sp2.text = 'Selecting course';
-            await frame.waitForNavigation({waitUntil: "domcontentloaded"});
-
+            
             // @ts-ignore
             await frame.evaluate(() => {
                 'use strict';
@@ -86,65 +84,72 @@ const getPassword = async (): Promise<string> =>
                 const jCdIndex = table.find('thead > tr').find('th:contains("時間割コード")').index();
                 const courses=table.find('tbody > tr').not(':has(a)');
                 const codes = courses.find(`td:nth-of-type(${jCdIndex+1})`);
-                console.log(courses);
                 const links = courses.find('td:nth-last-of-type(2)').wrapInner('<a></a>').children('a');
-                console.log(year, shozokuCd, lang);
                 links.attr({onclick: (i) => `refer('${year}','${shozokuCd}','${codes[i].innerText}','${lang}');`}).css({'color': '#027075'});
             });
+
+            await frame.waitForNavigation({waitUntil: "domcontentloaded"});
             await frame.waitFor(1000);
+            
+            sp2.succeed('Opened course list');
 
-            let elist = await frame.$x(`/html/body/table/tbody/tr/td/a[contains(text(), "${courseName}")]`);
-            if (elist.length === 0) {throw Error(`No such course.`)}
-            if (elist.length > 1) {
-                const exact = (await Promise.all(elist.map(async e => await getInnerText(e)))).filter(s => s === courseName);
-                if (exact.length === 0) { throw Error(`No exact match.`)}
-                if (exact.length > 1) { throw Error(`Multiple exact match.`)} // Is this possible?
+            for (const courselink of (await frame.$$('td > a'))/*.slice(0, 3)*/) {
+                let sp3 = ora({text: 'Opening course page', indent: 2}).start();
+                let courseName: string = null;
+                try {
+                    courseName = await (await courselink.getProperty('innerText')).jsonValue() as string;
+                    console.log(`Scraping ${gakubu} > ${gakka} > ${courseName}`);
+
+                    const coursePagePromise: Promise<puppeteer.Page> = new Promise((resolve, reject) => {
+                        browser.once('targetcreated', async target => resolve(await target.page()));
+                    });
+
+                    await courselink.click();
+                    const coursePage = await coursePagePromise;
+
+                    sp3.text = 'Collecting data';
+                    
+                    await coursePage.waitFor(`#tabs-1 > table > tbody > tr`);
+                    await coursePage.waitFor(`#tabs-2 > table > tbody > tr`);
+                    await page.waitFor(1000);
+                    const data: string[][]  = (await Promise.all(
+                        (await Promise.all([1,2].map(async (i) => await coursePage.$$(`#tabs-${i} > table > tbody > tr`)))).flat()
+                        .map(async e =>
+                            await Promise.all(
+                                (await Promise.all(
+                                    ['th', 'td'].map(async tag => await e.$(tag))
+                                ))
+                                .map(async e => e === null ? '': await getInnerText(e))
+                            )
+                        )
+                    )).map(l => l.map(s => s.trim().replace(/／.+?(　|$)/g, '').trim()));
+                
+                    await coursePage.waitFor('#referInputForm');
+                    await page.waitFor(1000);
+                    const {code, nendo}: {code: string, nendo: string} = await coursePage.$eval('#referInputForm', (form) => {
+                        const formData = form.elements;
+                        return {
+                            code: formData.jikanwaricd.value,
+                            nendo: formData.nendo.value,
+                        }
+                    }) as any;
+                    const permalink =  `https://catalog.he.u-tokyo.ac.jp/detail?code=${code}&year=${nendo}`;
+                    
+                    data.push(['シラバスURL', permalink]);
+
+                    sp3.text = 'Saving data';
+                    await fs.writeFile(path.join(__dirname, 'syllabus_data_secret', `${code}.json`), JSON.stringify(data));
+
+                    sp3.succeed('Saved!');
+                    
+                    await coursePage.close();
+                } catch (e) {
+                    sp3.fail(`\nFailed to scrape ${gakubu} > ${gakka} > ${courseName}`);
+                    console.error(e);
+                }
             }
-
-            const coursePagePromise: Promise<puppeteer.Page> = new Promise((resolve, reject) => {
-                browser.once('targetcreated', async target => resolve(await target.page()));
-            });
-            await page.waitFor(1000);
-
-            sp2.text = 'Opening course page';
-            await elist[0].click();
-            const coursePage = await coursePagePromise;
-
-            sp2.text = 'Collecting data';
-            
-            await coursePage.waitFor(`#tabs-1 > table > tbody > tr`);
-            await coursePage.waitFor(`#tabs-2 > table > tbody > tr`);
-            await page.waitFor(1000);
-            const data: string[][]  = (await Promise.all(
-                (await Promise.all([1,2].map(async (i) => await coursePage.$$(`#tabs-${i} > table > tbody > tr`)))).flat()
-                .map(async e =>
-                    await Promise.all(
-                        (await Promise.all(
-                            ['th', 'td'].map(async tag => await e.$(tag))
-                        ))
-                        .map(async e => e === null ? '': await getInnerText(e))
-                    )
-                )
-            )).map(l => l.map(s => s.trim().replace(/／.+?(　|$)/g, '').trim()));
-        
-            await coursePage.waitFor('#referInputForm');
-            // @ts-ignore
-            const permalink: string = await coursePage.$eval('#referInputForm', (f): string => {
-                // @ts-ignore
-                const formData = document.forms.referInputForm.elements;
-                return `https://catalog.he.u-tokyo.ac.jp/detail?code=${formData.jikanwaricd.value}&year=${formData.nendo.value}`;
-            }) as string;
-            
-            data.push(['シラバスURL', permalink]);
-
-            sp2.text = 'Saving data';
-            await fs.writeFile(path.join(__dirname, 'syllabus_data_secret', `${courseName}.json`), JSON.stringify(data));
-
-            sp2.succeed('Saved!');
-            
-            await coursePage.close();
         } catch (e) {
-            console.error(`\nFailed to scrape ${gakubu} > ${gakka} > ${courseName}`);
+            sp2.fail(`\nFailed to scrape ${gakubu} > ${gakka}`);
             console.error(e);
         }
     }
