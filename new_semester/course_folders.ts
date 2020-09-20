04a8631a8ad64302d220c0e145d391b4b8f1973d
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2020-09-20 18:48:38 +0900

    new_semester/course_folders: update

--- new_semester/course_folders.ts
+++ new_semester/course_folders.ts
@@ -3,8 +3,17 @@ import {getGoogleClient} from '../utils/google';
 
 import { promises as fs } from 'fs';
 import path from 'path';
-import { z2h, splitString } from '../utils/utils';
-import { alias, groupEmailAddress, rootFolderId } from './course_folders.secret';
+import { z2h, splitString, sleep } from '../utils/utils';
+import { alias, groupEmailAddress, rootFolderId } from './course_folders_secret';
+
+const shouldSkip = (course: Map<string, string>): boolean => {
+    if (course.get('学年').includes('B2')) return true;
+    const sem = course.get('開講区分');
+    if ((sem.includes('S') || sem.includes('Ｓ')) && !(sem.includes('A') || sem.includes('Ａ'))) return true;
+    if ((!course.get('共通科目コード').startsWith('FEN')) && course.get('他学部履修') !== '可') return true;
+
+    return false;
+}
 
 (async () => {
     const auth = await getGoogleClient();
@@ -13,18 +22,40 @@ import { alias, groupEmailAddress, rootFolderId } from './course_folders.secret'
     const folder = path.join(__dirname, 'syllabus_data_secret');
     const files = await fs.readdir(folder);
     const skipList = ''.split(' ');
+
+    const existingFolders = (await drive.files.list({
+        q: `'${rootFolderId}' in parents and mimeType = 'application/vnd.google-apps.folder'`,
+    })).data.files;
+    const existingFolderNames = new Set(existingFolders.map(({name}) => name));
+
     for (const file of files) {
         try {
             if (skipList.includes(file.replace('.json', ''))) {continue;}
             const fullpath = path.join(folder,file);
             const folderMimeType = 'application/vnd.google-apps.folder';
             const data = new Map(JSON.parse(await fs.readFile(fullpath, {encoding: 'utf-8'}))) as Map<string, string>;
-            const yougen = z2h(data.get('曜限').split(/,\s+/g).join('').replace(/\s/g,''));
+            if (shouldSkip(data)) {
+                // console.log('skipping', data.get('開講科目名'), file)
+                continue;
+            }
+            const yougens = splitString(data.get('曜限').split(/,\s+/g).join('').replace(/\s/g,''), 2);
+            const yougenMap = new Map<string, string[]>();
+            for (const yougen of yougens) {
+                const you = yougen[0];
+                const gen = yougen[1];
+                const youData = yougenMap.get(you) || [];
+                youData.push(gen);
+                yougenMap.set(you, youData);
+            }
+            const youList = '月火水木金集'.split('')
+                .filter(you => yougenMap.has(you));
+            const yougenString = youList
+                .map(you => `${you}${yougenMap.get(you).sort().join('')}`)
+                .join('');
             const name = z2h(data.get('開講科目名'));
-            const youSet = new Set(splitString(yougen, 2).map(s => s.charAt(0)));
-            const you = '月火水木金集'.split('').filter(s => youSet.has(s)).join('');
             // create course folder
-            const folderName = `${("月火水木金".indexOf(yougen.charAt(0)) + 1)||9}${/^[0-9]$/.test(yougen.charAt(1))?yougen.charAt(1):9}${you}-${alias.get(name)||name}`
+            const folderName = `${"月火水木金集".indexOf(youList[0]) + 1}-${yougenString}${z2h(name)}`
+            if (existingFolderNames.has(folderName)) {continue;}
             console.log(folderName);
             const courseFolderId = (await drive.files.create({
                 requestBody: {
@@ -55,7 +86,8 @@ import { alias, groupEmailAddress, rootFolderId } from './course_folders.secret'
                 }
             });
             console.log(`${name}\t${courseFolderId}`);
-            res.push([file, courseFolderId]);
+            res.push([file.replace('.json', ''), courseFolderId]);
+            await sleep(5000);
         } catch (e) {
             console.error('Error in', file)
             console.error(e);
